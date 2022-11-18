656c04e41b11d6e119ad69ea4a2063540958b5fb
mohammad eydi <mohammadeydi2020@gmail.com>
2022-11-18 12:49:14 +0330

    inline style trained

--- src/components/search.js
+++ src/components/search.js
@@ -5,23 +5,34 @@ class search extends Component {
     divStyle ={
         backgroundColor : "#5f25bc",
         width : "100%",
-        height:"100px",
+        height:"160px",
         display : "flex",
         justifyContent : "space-around" ,
         alignItems:"center"    ,
+       
         ":hover":{
             backgroundColor : "purple",
         },
-        "@media (max-width : 600px)":{
+        "@media (max-width : 1000px)":{
             flexDirection:"column"
         }
     }
+    inputStyle={
+        marginBottom : "10px",
+        height:"30px",
+        borderRadius:"50px",
+        border:"none",
+        padding:"10px",
 
+    }
     render() {
         return (
             <div style={this.divStyle} >
                 <h2 >search what you want</h2>
-                <input  type="text" name="name" />
+                <div>
+                <input style={this.inputStyle} type="text" name="name" />
+                <button style={{width:"50px",height:"50px",borderRadius:"50%",border:"none",marginLeft:"5px"}}>ok</button>
+                </div>
             </div>
         );
     }
