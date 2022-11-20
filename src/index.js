792babb821b0941b6e5d7de0d73136fc38f6f75b
mohammad eydi <mohammadeydi2020@gmail.com>
2022-11-20 18:39:29 +0330

    burger deleted

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App';
-
-
-
-
-const rootMaker = ReactDOM.createRoot(document.getElementById("root"))
-rootMaker.render(<App />);
