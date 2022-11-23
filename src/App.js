dc69153ed541e6936d91946d28c653d987099d70
mohammad eydi <mohammadeydi2020@gmail.com>
2022-11-23 14:25:34 +0330

    worked with events

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ClassComponent from './classComponent';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div><ClassComponent/></div>
+    )
+  }
+}
+
+export default App;
